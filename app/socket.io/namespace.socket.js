51e5e769f9e21cb14c97fefd2421bfea41f4d1a1
aminvv <aminviper1378@gmail.com>
2025-07-26 17:56:08 +0330

    Sending namespace to client and displaying on client side part one

--- app/socket.io/namespace.socket.js
+++ app/socket.io/namespace.socket.js
@@ -1,3 +1,5 @@
+const {ConversationModel} = require("../models/conversation.model")
+
 module.exports= class NamespaceSocketHandler {
     #io
     constructor(io){
@@ -5,8 +7,28 @@ module.exports= class NamespaceSocketHandler {
     }
 
     initConnection(){
-        this.#io.on("connection",socket=>{
-            console.log(socket.rooms);
+        this.#io.on("connection",async socket=>{
+            const namespace=await ConversationModel.find({},{title:1,endpoint:1}).sort({_id:-1})
+            socket.emit("namespaceList",namespace)
         })
     }
-}
+
+
+
+    async createNamespaceConnection(){
+            const namespace=await ConversationModel.find({},{title:1,endpoint:1, rooms:1}).sort({_id:-1})
+        for (const namespace of namespaces) {
+                this.#io.of(`/${namespace.endpoint}`).on("connection",()=>{
+            socket.emit("namespaceList",namespace.rooms)
+                })
+        }
+        }
+
+
+
+
+
+    }
+
+
+
--- public/dist/socket.js
+++ public/dist/socket.js
@@ -1,4 +1,17 @@
- const socket = io("http://localhost:3000");
+
+const socket = io("http://localhost:3000");
 
 
- socket.on("connect",()=>{})
+socket.on("connect", () => {
+    socket.on("namespaceList", namespaceList => {
+        const namespaceElement = document.getElementById("namespaces")
+           namespaceElement.innerHTML = ""
+         for (const namespace of namespaceList) {
+            const li = document.createElement("li");
+            const p = document.createElement("p")
+            p.innerText = namespace.title
+            li.appendChild(p)
+            namespaceElement . appendChild(li)
+        }
+    })
+})   
